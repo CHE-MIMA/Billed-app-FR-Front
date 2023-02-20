bf56a593c991c79fcdb0e6b628f9bf1778f1ce8a
mima ch <mima.cherfa19@hotmail.fr>
2023-02-20 14:37:48 +0100

    ajout test bills et new bills

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 
+
 ## L'architecture du projet :
 Ce projet, dit frontend, est connecté à un service API backend que vous devez aussi lancer en local.
 
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-
+import BillsUI from "../views/BillsUI"
 import { fireEvent, screen, waitFor } from "@testing-library/dom";
 import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
@@ -64,6 +64,89 @@ describe("when I submit the form with empty fields", () => {
     expect(form).toBeTruthy();
   });
 });
+describe("When I select an image in a correct format", () => {
+  test("Then the input file should display the file name", () => {
+    //page NewBill
+    const html = NewBillUI();
+    document.body.innerHTML = html;
+    const onNavigate = (pathname) => {
+      document.body.innerHTML = ROUTES({ pathname });
+    };
+    // initialisation NewBill
+    const newBill = new NewBill({
+      document,
+      onNavigate,
+      mockStore,
+      localStorage: window.localStorage,
+    });
+    const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e));
+    const input = screen.getByTestId("file");
+    input.addEventListener("change", handleChangeFile);
+    //fichier au bon format
+    fireEvent.change(input, {
+      target: {
+        files: [
+          new File(["image.png"], "image.png", {
+            type: "image/png",
+          }),
+        ],
+      },
+    });
+    expect(handleChangeFile).toHaveBeenCalled();
+    expect(input.files[0].name).toBe("image.png");
+  });
+  test("Then a bill is created", () => {
+    //page NewBill
+    const html = NewBillUI();
+    document.body.innerHTML = html;
+    const onNavigate = jest.fn();
+    // initialisation NewBill
+    const newBill = new NewBill({
+      document,
+      onNavigate,
+      mockStore: null,
+      localStorage: window.localStorage,
+    });
+    //fonctionnalité submit
+    const submit = screen.getByTestId("form-new-bill");
+    fireEvent.submit(submit);
+    expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH["Bills"]);
+  });
+});
+describe("When I select a file with an incorrect extension", () => {
+  test("Then the bill is deleted", () => {
+    //page NewBill
+    const html = NewBillUI();
+    document.body.innerHTML = html;
+    const onNavigate = (pathname) => {
+      document.body.innerHTML = ROUTES({ pathname });
+    };
+    // initialisation NewBill
+    const newBill = new NewBill({
+      document,
+      onNavigate,
+      mockStore: null,
+      localStorage: window.localStorage,
+    });
+    // fonctionnalité séléction fichier
+    const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e));
+    const input = screen.getByTestId("file");
+    input.addEventListener("change", handleChangeFile);
+    //fichier au mauvais format
+    fireEvent.change(input, {
+      target: {
+        files: [
+          new File(["image.txt"], "image.txt", {
+            type: "image/txt",
+          }),
+        ],
+      },
+    });
+    expect(handleChangeFile).toHaveBeenCalled();
+    expect(input.files[0].name).toBe("image.txt");
+  });
+});
+
 //test d'intégration POST
 
 describe("Given I am connected as Employee on NewBill page, and submit the form", () => {
@@ -86,6 +169,77 @@ describe("Given I am connected as Employee on NewBill page, and submit the form"
     router();
   });
 
+  describe("when APi is working well", () => {
+    test("then i should be sent on bills page with bills updated", async () => {
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        mockStore,
+        localStorage: localStorageMock,
+      });
+
+      const form = screen.getByTestId("form-new-bill");
+      const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
+      form.addEventListener("submit", handleSubmit);
+
+      fireEvent.submit(form);
+
+      expect(handleSubmit).toHaveBeenCalled();
+      expect(screen.getByText("Mes notes de frais")).toBeTruthy();
+      expect(mockStore.bills).toHaveBeenCalled();
+    });
+    describe("When an error occurs on API", () => {
+      beforeEach(() => {
+        jest.spyOn(mockStore, "bills");
+        Object.defineProperty(window, "localStorage", {
+          value: localStorageMock,
+        });
+        window.localStorage.setItem(
+          "user",
+          JSON.stringify({
+            type: "Employee",
+            email: "a@a",
+          })
+        );
+        const root = document.createElement("div");
+        root.setAttribute("id", "root");
+        document.body.appendChild(root);
+        router();
+      });
+      test("fetches bills from an API and fails with 404 message error", async () => {
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => {
+              return Promise.reject(new Error("Erreur 404"));
+            },
+          };
+        });
+        window.onNavigate(ROUTES_PATH.Bills);
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 404/);
+        expect(message).toBeTruthy();
+      });
+
+      test("fetches messages from an API and fails with 500 message error", async () => {
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => {
+              return Promise.reject(new Error("Erreur 500"));
+            },
+          };
+        });
+
+        window.onNavigate(ROUTES_PATH.Bills);
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 500/);
+        expect(message).toBeTruthy();
+      });
+    });
+  });
+
+
+
+
   describe("When an error occurs on API", () => {
     test("then it should display a message error", async () => {
       console.error = jest.fn();
@@ -101,7 +255,7 @@ describe("Given I am connected as Employee on NewBill page, and submit the form"
       const newBill = new NewBill({
         document,
         onNavigate,
-        store: mockStore,
+        mockStore,
         localStorage: window.localStorage,
       });
 
@@ -119,7 +273,6 @@ describe("Given I am connected as Employee on NewBill page, and submit the form"
     });
   });
 });
-
 
 
 
